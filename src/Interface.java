8980712d493ed1b8c866cd83e126490cd033fe02
Thanaen <Clem0970@gmail.com>
2017-11-09 16:48:44 +0100

    - Retrait d'un bouton

--- src/Interface.java
+++ src/Interface.java
@@ -8,54 +8,47 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 public class Interface extends JFrame {
+	private static final long serialVersionUID = 1L;
+
 	public Interface() {
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Jeu du coffre");
 		getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
-		
+
 		JButton btnJouer = new JButton("Jouer");
 		btnJouer.addActionListener(actionJouer());
 		btnJouer.setAlignmentX(Component.CENTER_ALIGNMENT);
 		getContentPane().add(btnJouer);
-		
-		JButton btnCharger = new JButton("Charger niveau");
-		btnJouer.addActionListener(actionCharger());
-		btnCharger.setAlignmentX(Component.CENTER_ALIGNMENT);
-		getContentPane().add(btnCharger);
-		
+
 		JButton btnCopyrights = new JButton("Credits");
-		btnCopyrights.addActionListener(actionCopy());
+		btnJouer.addActionListener(actionCopy());
 		btnCopyrights.setAlignmentX(Component.CENTER_ALIGNMENT);
 		getContentPane().add(btnCopyrights);
-		
+
 		JButton btnQuitter = new JButton("Quitter");
 		btnQuitter.addActionListener(actionQuitter());
 		btnQuitter.setAlignmentX(Component.CENTER_ALIGNMENT);
 		getContentPane().add(btnQuitter);
 	}
 
-    private ActionListener actionQuitter() {
+	private ActionListener actionQuitter() {
 		// TODO Auto-generated method stub
-    	System.exit(0);
-		return null;
-	}
-
-	private ActionListener actionCopy() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private ActionListener actionCharger() {
-		// TODO Auto-generated method stub
+		System.exit(0);
 		return null;
 	}
 
 	private ActionListener actionJouer() {
 		// TODO Auto-generated method stub
+
+		dispose();
 		return null;
 	}
 
-	private static final long serialVersionUID = 1L;
-
+	private ActionListener actionCopy() {
+		JOptionPane.showMessageDialog(null,
+				"Le Jeu du coffre a été réalisé par Basil Soubrevilla, Loïc Barbé et Clément Costedoat, le 10/11/2017.");
+		return null;
+	}
 
 }
